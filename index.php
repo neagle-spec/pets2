d94aeb0d7ee2d1d644cc6b47ce8c2b9564d68f56
neagle-spec <neagleaaron@hotmail.com>
2022-05-05 12:20:28 -0400

    Created composer.json and index.php files

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+// turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// require the autoload file
+require_once('vendor/autoload.php');
+
+// create an instance of the Base class
+$f3 = Base::instance();
+
+// define a default root
+$f3->route('GET /', function ($f3) {
+    echo '<h1>My Pets</h1>';
+    $view = new Template();
+    echo $view->render('views/info.html');
+});
+
+
+
+// run fat free
+$f3->run();
