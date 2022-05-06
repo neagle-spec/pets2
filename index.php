f3c818a2fc2b156079c3ae608acb78983a641616
night780 <night780j@gmail.com>
2022-05-06 18:26:15 -0400

    form collection bug resolved

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
+//Starts session
+session_start();
 // require the autoload file
 require_once('vendor/autoload.php');
 
@@ -12,42 +14,40 @@ $f3 = Base::instance();
 
 // define a default root
 $f3->route('GET /', function () {
-    echo '<h1>My Pets</h1>';
+
     $view = new Template();
     echo $view->render('views/pet-home.html');
 });
 
-$f3->route('GET /order', function () {
-    echo '<h1>Order a Pet</h1>';
+// define a home root
+$f3->route('GET /home', function () {
+
+    $view = new Template();
+    echo $view->render('views/pet-home.html');
+});
+
+$f3->route('GET|POST /order', function () {
+
     $view = new Template();
     echo $view->render('views/pet-order.html');
 });
 
-$f3->route('GET /order1', function () {
+$f3->route('GET|POST /order1', function () {
 
     $view = new Template();
     echo $view->render('views/pet-order1.html');
 });
 
-$f3->route('POST /order2', function () {
+$f3->route('GET|POST /order2', function () {
     var_dump ($_POST);
-    $_SESSION['petType'] = $_POST['petType'];
-    $_SESSION['color'] = $_POST['color'];
-    $_SESSION['name'] = $_POST['name'];
+    $_SESSION['petType1'] = $_POST['petType1'];
+    $_SESSION['color1'] = $_POST['color1'];
     $view = new Template();
     echo $view->render('views/pet-order2.html');
 });
 
 $f3->route('GET|POST /summary', function () {
-    var_dump ($_POST);
-    $_SESSION['petType'] = $_POST['petType'];
-    $_SESSION['color'] = $_POST['color'];
-    if (empty($_POST['name'])) {
-        $name = "No Hobby selected";
-    }
-    else {
-    }
-    $_SESSION['name'] = $name;
+    $_SESSION['name'] = $_POST['name'];
 
     $view = new Template();
     echo $view->render('views/pet-summary.html');
